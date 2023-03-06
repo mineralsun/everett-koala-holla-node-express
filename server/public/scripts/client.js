b68e67f6da9d69a2e3403255b5a2d4c009093187
Everett Butler <everettjamesbutler@gmail.com>
2023-03-05 19:17:55 -0600

    got my koalas to show on the dom and to add new koalas working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,14 +2,62 @@ console.log( 'js' );
 
 function getKoalas(){
   console.log( 'in getKoalas' );
+  axios.get('/koalas').then((response) => {
+    console.log(response);
+    let koalasFromServer = response.data;
+    let contentDiv = document.querySelector('#viewKoalas');
+    contentDiv.innerHTML = '';
+    for(let koala of koalasFromServer)
+      contentDiv.innerHTML += `
+      <tr>
+        <td>${koala.id}</td>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.readyToTransfer}</td>
+        <td>${koala.notes}</td>
+        <td><button id="deleteButton" onClick="deleteKoala(event)">Delete</button><td>
+      </tr>
+      `;
+  }).catch((error) => {
+    console.log(error);
+    alert('Something went wrong!')
+  });
   // axios call to server to get koalas
   
 } // end getKoalas
 
-function saveKoala(){
-  console.log( 'in saveKoala' );
-  // axios call to server to get koalas
+
+
+//TODOfunction saveKoala(){
+  //TODOconsole.log( 'in saveKoala' );
+  //TODO axios call to server to get koalas
  
-}
+//TODO}
 
 getKoalas();
+
+function addKoala(event) {
+  event.preventDefault();
+  let koalaId = document.querySelector('#IDin').value;
+  let koalaName = document.querySelector('#nameIn').value;
+  let koalaAge = document.querySelector('#ageIn').value;
+  let koalaGender = document.querySelector('#genderIn').value;
+  let koalaReadyToTransfer = document.querySelector('#readyForTransferIn').value;
+  let koalaNotes = document.querySelector('#notesIn').value;
+  let koalaForServer = {
+    id: koalaId,
+    name: koalaName,
+    gender: koalaGender,
+    age: koalaAge,
+    readyToTransfer: koalaReadyToTransfer,
+    notes: koalaNotes,
+  };
+  axios.post('/koalas', koalaForServer).then((response) => {
+    console.log(response);
+    getKoalas();
+  }).catch((error) => {
+    console.log(error);
+    alert('Something went wrong')
+  })
+}
