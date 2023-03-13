6b7bd99b0cd31b96d8e9c5629f8130d1a9a018fd
Everett Butler <everettjamesbutler@gmail.com>
2023-03-12 19:09:10 -0500

    got pool and router GETs and POSTs working

---
+++ server/modules/pool.js
@@ -0,0 +1,13 @@
+const pg = require('pg');
+
+const pool = new pg.Pool({
+
+    database: 'koala',
+
+    host: 'localhost',
+
+    port: 5432,
+
+});
+
+module.exports = pool;
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,73 +1,87 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pool = require('../modules/pool.js');
 
-let koalas = [
-    {
-      id: 1,
-      name: 'Scotty',
-      gender: 'M',
-      age: 4,
-      readyToTransfer: 'Yes',
-      notes: 'Born in Guatemala',
-    },
-    {
-      id: 2,
-      name: 'Jean',
-      gender: 'F',
-      age: 5,
-      readyToTransfer: 'Yes',
-      notes: 'Allergic to lots of lava',
-    },
-    {
-      id: 3,
-      name: 'Ororo',
-      gender: 'F',
-      age: 7,
-      readyToTransfer: 'No',
-      notes: 'Loves listening to Paula (Abdul)',
-    },
-    {
-      id: 4,
-      name: 'Logan',
-      gender: 'M',
-      age: 15,
-      readyToTransfer: 'No',
-      notes: 'Loves the sauna',
-    },
-    {
-      id: 5,
-      name: 'Charlie',
-      gender: 'M',
-      age: 9,
-      readyToTransfer: 'Yes',
-      notes: 'Favorite band is Nirvana',
-    },
-    {
-      id: 6,
-      name: 'Betsy',
-      gender: 'F',
-      age: 4,
-      readyToTransfer: 'Yes',
-      notes: 'Has a pet iguana'
-    },
-  ];
+// let koalas = [
+//     {
+//       id: 1,
+//       name: 'Scotty',
+//       gender: 'M',
+//       age: 4,
+//       readyToTransfer: 'Yes',
+//       notes: 'Born in Guatemala',
+//     },
+//     {
+//       id: 2,
+//       name: 'Jean',
+//       gender: 'F',
+//       age: 5,
+//       readyToTransfer: 'Yes',
+//       notes: 'Allergic to lots of lava',
+//     },
+//     {
+//       id: 3,
+//       name: 'Ororo',
+//       gender: 'F',
+//       age: 7,
+//       readyToTransfer: 'No',
+//       notes: 'Loves listening to Paula (Abdul)',
+//     },
+//     {
+//       id: 4,
+//       name: 'Logan',
+//       gender: 'M',
+//       age: 15,
+//       readyToTransfer: 'No',
+//       notes: 'Loves the sauna',
+//     },
+//     {
+//       id: 5,
+//       name: 'Charlie',
+//       gender: 'M',
+//       age: 9,
+//       readyToTransfer: 'Yes',
+//       notes: 'Favorite band is Nirvana',
+//     },
+//     {
+//       id: 6,
+//       name: 'Betsy',
+//       gender: 'F',
+//       age: 4,
+//       readyToTransfer: 'Yes',
+//       notes: 'Has a pet iguana'
+//     },
+//   ];
   
 // DB CONNECTION
 
 
 // GET
 koalaRouter.get('/', (req, res) => {
-    console.log('GET request made for Koalas');
-    res.send(koalas);
+    console.log('GET request made for koalas');
+    let queryText = 'SELECT * FROM "koalas";';
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error in GET ${error}`);
+        alert(`Something went wrong`);
+        res.sendStatus(500);
+    });
 });
 
 // POST
 koalaRouter.post('/', (req, res) => {
-    console.log('POST request made for Koalas');
+    console.log('POST request made for koalas');
     console.log(req.body);
     let koalaToAdd = req.body;
-    koalas.push(koalaToAdd);
-    res.sendStatus(201);
+    let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "readyToTransfer", "notes")
+                     VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText, [koalaToAdd.name, koalaToAdd.gender, koalaToAdd.age, koalaToAdd.readyToTransfer, koalaToAdd.notes]).then((result) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log(`Error in POST ${error}`);
+        res.sendStatus(500);
+    });
 });
 
 // PUT
